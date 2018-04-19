de427e063fcdd47eb7d34145724a23b36a89f931
BPeppard <bjpeppard@gmail.com>
2018-04-19 13:23:43 -0500

    Add Material-ui-next

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,83 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript } from 'next/document';
+import JssProvider from 'react-jss/lib/JssProvider';
+import flush from 'styled-jsx/server';
+import getPageContext from '../styles/getPageContext';
 
 export default class DefaultDocument extends Document {
-  static async getInitialProps (ctx) {
-    return await Document.getInitialProps(ctx)
+  static async getInitialProps(ctx) {
+    // return await Document.getInitialProps(ctx)
+    // Resolution order
+    //
+    // On the server:
+    // 1. page.getInitialProps
+    // 2. document.getInitialProps
+    // 3. page.render
+    // 4. document.render
+    //
+    // On the server with error:
+    // 2. document.getInitialProps
+    // 3. page.render
+    // 4. document.render
+    //
+    // On the client
+    // 1. page.getInitialProps
+    // 3. page.render
+
+    // Get the context of the page to collected side effects.
+    const pageContext = getPageContext();
+    const page = ctx.renderPage(Component => props => (
+      <JssProvider
+        registry={pageContext.sheetsRegistry}
+        generateClassName={pageContext.generateClassName}>
+        <Component pageContext={pageContext} {...props} />
+      </JssProvider>
+    ));
+
+    return {
+      ...page,
+      pageContext,
+      styles: (
+        <React.Fragment>
+          <style
+            id="jss-server-side"
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: pageContext.sheetsRegistry.toString()
+            }}
+          />
+          {flush() || null}
+        </React.Fragment>
+      )
+    };
   }
 
   render() {
+    const { pageContext } = this.props;
     /**
-    * Here we use _document.js to add a "lang" propery to the HTML object if
-    * one is set on the page.
-    **/
+     * Here we use _document.js to add a "lang" propery to the HTML object if
+     * one is set on the page.
+     **/
     return (
       <html lang={this.props.__NEXT_DATA__.props.lang || 'en'}>
         <Head>
+          <meta charSet="utf-8" />
+          {/* Use minimum-scale=1 to enable GPU rasterization */}
+          <meta
+            name="viewport"
+            content={
+              'user-scalable=0, initial-scale=1, ' +
+              'minimum-scale=1, width=device-width, height=device-height'
+            }
+          />
+          {/* PWA primary color */}
+          <meta
+            name="theme-color"
+            content={pageContext.theme.palette.primary.main}
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
         </Head>
         <body>
           {this.props.customValue}
@@ -20,6 +85,6 @@ export default class DefaultDocument extends Document {
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
---
+++ pages/examples/material-ui.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import classnames from 'classnames';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import Card, {
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions
+} from 'material-ui/Card';
+import Collapse from 'material-ui/transitions/Collapse';
+import Avatar from 'material-ui/Avatar';
+import red from 'material-ui/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import Page from '../../components/page';
+import Layout from '../../components/layout';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flex: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  card: {
+    maxWidth: 400
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%' // 16:9
+  },
+  actions: {
+    display: 'flex'
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest
+    }),
+    marginLeft: 'auto'
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)'
+  },
+  avatar: {
+    backgroundColor: red[500]
+  }
+});
+
+class MaterialUI extends Page {
+  state = {
+    expanded: false
+  };
+
+  handleExpandClick = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Layout {...this.props}>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="title"
+                color="inherit"
+                className={classes.flex}>
+                Title
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+        <div style={{ marginTop: 30 }}>
+          <Card className={classes.card}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="Recipe" className={classes.avatar}>
+                  R
+                </Avatar>
+              }
+              action={
+                <IconButton>
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title="Shrimp and Chorizo Paella"
+              subheader="September 14, 2016"
+            />
+            <CardMedia
+              className={classes.media}
+              image="/static/images/cards/paella.jpg"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography component="p">
+                This impressive paella is a perfect party dish and a fun meal to
+                cook together with your guests. Add 1 cup of frozen peas along
+                with the mussels, if you like.
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.actions} disableActionSpacing>
+              <IconButton aria-label="Add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton aria-label="Share">
+                <ShareIcon />
+              </IconButton>
+              <IconButton
+                className={classnames(classes.expand, {
+                  [classes.expandOpen]: this.state.expanded
+                })}
+                onClick={this.handleExpandClick}
+                aria-expanded={this.state.expanded}
+                aria-label="Show more">
+                <ExpandMoreIcon />
+              </IconButton>
+            </CardActions>
+            <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph variant="body2">
+                  Method:
+                </Typography>
+                <Typography paragraph>
+                  Heat 1/2 cup of the broth in a pot until simmering, add
+                  saffron and set aside for 10 minutes.
+                </Typography>
+                <Typography paragraph>
+                  Heat oil in a (14- to 16-inch) paella pan or a large, deep
+                  skillet over medium-high heat. Add chicken, shrimp and
+                  chorizo, and cook, stirring occasionally until lightly
+                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and
+                  set aside, leaving chicken and chorizo in the pan. Add
+                  pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                  pepper, and cook, stirring often until thickened and fragrant,
+                  about 10 minutes. Add saffron broth and remaining 4 1/2 cups
+                  chicken broth; bring to a boil.
+                </Typography>
+                <Typography paragraph>
+                  Add rice and stir very gently to distribute. Top with
+                  artichokes and peppers, and cook without stirring, until most
+                  of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                  medium-low, add reserved shrimp and mussels, tucking them down
+                  into the rice, and cook again without stirring, until mussels
+                  have opened and rice is just tender, 5 to 7 minutes more.
+                  (Discard any mussels that don’t open.)
+                </Typography>
+                <Typography>
+                  Set aside off of the heat to let rest for 10 minutes, and then
+                  serve.
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default withStyles(styles)(MaterialUI);
